1ccb7d481368122ab3f31c1d5f7b3bd7cf8b8e6f
Ako Bartani <bbi.d12@gmail.com>
2026-01-17 03:23:42 -0800

    Update dataset and output directory paths

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import torch
 import torchvision.transforms as transforms
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-Image_DIR = "D:/datasets"
-OUTPUT_DIR = "D:/results"
+Image_DIR = "datasets"
+OUTPUT_DIR = "results"
 mean=[0.5, 0.5, 0.5]
 std=[0.5, 0.5, 0.5]
 
